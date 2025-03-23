3545d9237e6c3d08c9fd5cf1d4898ecbf4697cd3
franchesoni <marchesoniacland@gmail.com>
2025-03-23 23:12:46 +0100

    new training with posneg

--- run_train.sh
+++ run_train.sh
@@ -7,7 +7,16 @@
 # 	     --gpus=0,1,2,3
 
 # train vitb-sax2 model on coco+lvis
-MODEL_CONFIG=./segnext/models/default/plainvit_base1024_cocolvis_sax2_1click.py
 export PYTHONPATH=$PYTHONPATH:/home/fmarchesoni/promptSeg
-torchrun --nproc-per-node=1 --master-port 29505 ./segnext/train.py ${MODEL_CONFIG} --batch-size=6 --gpus=1 --workers=48 
+## run 1click
+# MODEL_CONFIG=./segnext/models/default/plainvit_base1024_cocolvis_sax2_1click.py
+# torchrun --nproc-per-node=1 --master-port 29505 ./segnext/train.py ${MODEL_CONFIG} --batch-size=6 --gpus=1 --workers=48 
+## run 1click 512 resolution
+# MODEL_CONFIG=./segnext/models/default/plainvit_base1024_cocolvis_sax2_1click_512.py
+# torchrun --nproc-per-node=1 --master-port 29515 ./segnext/train.py ${MODEL_CONFIG} --batch-size=24 --gpus=0 --workers=48 
+## run 1 click pos neg at 512 resolution
+MODEL_CONFIG=./segnext/models/default/plainvit_base1024_cocolvis_sax2_1posneg_512.py
+torchrun --nproc-per-node=1 --master-port 29506 ./segnext/train.py ${MODEL_CONFIG} --batch-size=32 --gpus=0 --workers=48 
+## debug
+# torchrun --nproc-per-node=1 --master-port 29525 ./segnext/train.py ${MODEL_CONFIG} --batch-size=4 --gpus=0 --workers=0
  
